d56e61abf8e76ee652c1b151c3c7b40a7ac0517b
rgt320q <cetin.omer@outlook.com.tr>
2022-04-28 23:58:32 +0300

    Add project files.

---
+++ ECommerce.Api/Entities/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Api.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
